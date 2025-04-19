886a7f5da9a5d9c5d17ccd04a4574c04da435657
myousefg <myousefg@gmail.com>
2025-04-19 08:50:57 +0700

    mengantikan parameter dengan EmployeeInfo

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -25,27 +25,46 @@ public class TaxFunction {
 	 * 
 	 */
 
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
-			boolean isMarried, int numberOfChildren) {
+	public static int calculateTax(EmployeeInfo info) {
 
-		if (numberOfMonthWorking > 12) {
+		if (info.numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
 		}
 
-		if (numberOfChildren > MAX_CHILDREN_COUNTED) {
-			numberOfChildren = MAX_CHILDREN_COUNTED;
+		if (info.numberOfChildren > MAX_CHILDREN_COUNTED) {
+			info.numberOfChildren = MAX_CHILDREN_COUNTED;
 		}
 
-		int grossIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
+		int grossIncome = (info.monthlySalary + info.otherMonthlyIncome) * info.numberOfMonthWorking;
 
 		int tax;
-		if (isMarried) {
-			tax = (int) Math.round(TAX_RATE * (grossIncome - deductible
-					- (BASIC_NON_TAXABLE_INCOME + MARRIED_TAX_ALLOWANCE + (numberOfChildren * CHILD_TAX_ALLOWANCE))));
+		if (info.isMarried) {
+			tax = (int) Math.round(TAX_RATE * (grossIncome - info.deductible
+					- (BASIC_NON_TAXABLE_INCOME + MARRIED_TAX_ALLOWANCE
+							+ (info.numberOfChildren * CHILD_TAX_ALLOWANCE))));
 		} else {
-			tax = (int) Math.round(TAX_RATE * (grossIncome - deductible - BASIC_NON_TAXABLE_INCOME));
+			tax = (int) Math.round(TAX_RATE * (grossIncome - info.deductible - BASIC_NON_TAXABLE_INCOME));
 		}
 
 		return Math.max(tax, 0);
+	}
+
+	public static class EmployeeInfo {
+		public int monthlySalary;
+		public int otherMonthlyIncome;
+		public int numberOfMonthWorking;
+		public int deductible;
+		public boolean isMarried;
+		public int numberOfChildren;
+
+		public EmployeeInfo(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking,
+				int deductible, boolean isMarried, int numberOfChildren) {
+			this.monthlySalary = monthlySalary;
+			this.otherMonthlyIncome = otherMonthlyIncome;
+			this.numberOfMonthWorking = numberOfMonthWorking;
+			this.deductible = deductible;
+			this.isMarried = isMarried;
+			this.numberOfChildren = numberOfChildren;
+		}
 	}
 }
