946dd37d6ba9ba240fc3867c6c31243fd4d2ac56
myousefg <myousefg@gmail.com>
2025-04-19 08:31:31 +0700

    menghilangkan duplicate code pada perhitungan

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -22,8 +22,6 @@ public class TaxFunction {
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
 			boolean isMarried, int numberOfChildren) {
 
-		int tax = 0;
-
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
 		}
@@ -32,19 +30,16 @@ public class TaxFunction {
 			numberOfChildren = 3;
 		}
 
+		int grossIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
+
+		int tax;
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible
+			tax = (int) Math.round(0.05 * (grossIncome - deductible
 					- (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		} else {
-			tax = (int) Math.round(
-					0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+			tax = (int) Math.round(0.05 * (grossIncome - deductible - 54000000));
 		}
 
-		if (tax < 0) {
-			return 0;
-		} else {
-			return tax;
-		}
-
+		return Math.max(tax, 0);
 	}
 }
